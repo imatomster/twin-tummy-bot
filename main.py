eff6d315ca32f199b757fc822b447173a4e404f9
Tommy <imatomster@gmail.com>
2021-04-11 11:59:04 -0700

    Fixed no rank players and added opgg link

--- main.py
+++ main.py
@@ -25,29 +25,43 @@ bot_instance = DrinkEatFart()
 # All Commands
 @bot_instance.command(aliases=['TEST', 'TESTER'], help="I am just here to tell you if bot is working")
 async def test(ctx):
-    await ctx.reply('Hey there! Yes, I am working')
+    await ctx.send('Hey there! Yes, I am working')
 
 @bot_instance.command(aliases=['HELLO', 'hi', 'HI'], help="I am just here to say hi")
 async def hello(ctx):
     await ctx.trigger_typing()
-    await ctx.send('Hey bro')
+    await ctx.reply('Hey bro')
 
 @bot_instance.command(aliases=['mmr, MMR, league, OP.GG, OPGG, op.gg'], help="League Summoner Search")
 async def opgg(ctx, *, input):
     await ctx.trigger_typing()
 
     try:
+        # Testing if summoner exists
         user = watcher.summoner.by_name('na1', input)
     except ApiError as err:
-        await ctx.send(f'Sorry bro but something wrong with {input}')
+        # If it doesn't > ApiError 404 and we can respond
+        await ctx.send(f'Are you sure {input} exists on the rift?')
     else:
-        user = watcher.summoner.by_name('na1', input) 
-        stats = watcher.league.by_summoner('na1', user['id'])[0]
-        summonerName = stats['summonerName']
-        tier = stats['tier']
-        rank = stats['rank']
-        lp = stats['leaguePoints']
-        await ctx.send(f'Username: {summonerName}\nRank: {tier} {rank} and {lp} lp')
+        # Create User and Check Stats
+        user = watcher.summoner.by_name('na1', input)
+        stats = watcher.league.by_summoner('na1', user['id'])
+
+        # Check if they have enough information for stats
+        if len(stats) > 0: # The stats array will be empty if not enough games
+            stats = stats[0]
+            summonerName = stats['summonerName']
+            tier = stats['tier']
+            rank = stats['rank']
+            lp = stats['leaguePoints']
+            userLink = summonerName.replace(' ', '')
+            await ctx.send(f'Username: {summonerName}\nRank: {tier} {rank} and {lp} lp \nFor more info: https://na.op.gg/summoner/userName={userLink}')
+        else:
+            summonerName = user['name']
+            userLink = summonerName.replace(' ', '')
+            await ctx.send(f'It seems that {summonerName} does not have a rank!\nFor more info: https://na.op.gg/summoner/userName={userLink}')
+
+        
 
 # Execute Instance
 bot_instance.run(TOKEN_DISCORD)
