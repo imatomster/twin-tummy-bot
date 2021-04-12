237aca0c84470feaadd7a3ca55fb757a4aa3895b
Tommy <imatomster@gmail.com>
2021-04-12 10:57:22 -0700

    new functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from secret import TOKEN_DISCORD, TOKEN_RIOTWATCHER
 
 # Import discord and then the specific commands from extra discord
+import discord
 from discord.ext import commands
 # Import riotwatcher
 from riotwatcher import LolWatcher, ApiError
@@ -9,20 +10,25 @@ from riotwatcher import LolWatcher, ApiError
 watcher = LolWatcher(TOKEN_RIOTWATCHER)
 
 # Import requests to get something from fox website
-import requests
+import requests, random
 
 # Custom Bot Constructor for my Discord Bot
 class twin_tummy(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='$')
 
-    async def on_ready(self):
-        # on launch > tell console that I launched
-        print('Online as', self.user)
-        # print('self', dir(self)) to see all commands
+    # async def on_ready(self):
+    #     print('Online as', self.user)
+    #     # print('self', dir(self)) to see all commands
 
 # Instancing a Bot
 bot_instance = twin_tummy()
+
+# All Events
+@bot_instance.event
+async def on_ready():
+    print('Online as', bot_instance.user)
+    await bot_instance.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="CURTIS CUZ I LOVE YOU"))
 
 # All Commands
 @bot_instance.command(aliases=['TEST', 'TESTER'], help="I am just here to tell you if bot is working")
@@ -38,7 +44,45 @@ async def hello(ctx):
 async def fox(ctx):
     picture = requests.get('http://randomfox.ca/floof').json()['image']
     discord_id = '<@383129219326017569>'
-    await ctx.send(f'{discord_id}\n{picture}')    
+    await ctx.send(f'{discord_id}\n{picture}')
+
+@bot_instance.command(aliases=['MEME'], help="Is today monday?")
+async def meme(ctx):
+    await ctx.trigger_typing()
+    picture = requests.get('https://memeapi.pythonanywhere.com/').json()['memes'][0]['url']
+    await ctx.send(f'Is today Monday?\n{picture}')
+
+@bot_instance.command(help='This is not rickroll I promise')
+async def rickroll(ctx):
+    await ctx.send('https://tenor.com/view/rick-astley-rick-roll-dancing-dance-moves-gif-14097983')
+
+@bot_instance.command(help='Alexa Play despacito')
+async def despacito(ctx):
+    await ctx.send('https://www.youtube.com/watch?v=whwe0KD_rGw')
+
+@bot_instance.command(aliases=['8ball', 'ASK', 'question'], help='I can predict the future')
+async def ask(ctx, *, question):
+    responses = ['It is certain :eyes:.',
+                 'It is decidedly so :eyes:.',
+                 'Without a doubt :eyes:.',
+                 'Yes - definitely :eyes:',
+                 'You may rely on it :eyes:',
+                 'As I see it, yes :eyes:.',
+                 'Most likely :eyes:',
+                 'Outlook good :eyes:.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now',
+                 'Concentrate and ask again.',
+                 "Don't count on it.",
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 @bot_instance.command(aliases=['mmr', 'MMR', 'league', 'OP.GG', 'OPGG', 'op.gg'], help="League Summoner Search")
 async def opgg(ctx, *, input):
